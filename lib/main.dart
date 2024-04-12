11e5bcabd9562e50d79d663ec6170fef847aeac7
Mohammed Natour <bgd4500@gmail.com>
2024-04-12 19:29:46 +0300

    feature: add fonts and colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:ez_english/theme/palette.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +12,71 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: const [
+                CustomButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatefulWidget {
+  const CustomButton({super.key});
+
+  @override
+  State<CustomButton> createState() => CustomButtonState();
+}
+
+class CustomButtonState extends State<CustomButton> {
+  var isPressed = false;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (details) {
+        setState(() {
+          isPressed = true;
+        });
+      },
+      onTapUp: (details) {
+        setState(() {
+          isPressed = false;
+        });
+      },
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: Palette.primary,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: isPressed
+              ? null
+              : [
+                  BoxShadow(
+                    color: Palette.primaryShadow,
+                    offset: Offset(0, 2),
+                    blurRadius: 0,
+                  ),
+                ],
+        ),
+        width: 300,
+        height: 48,
+        child: Center(
+          child: Text(
+            "CONTINUE",
+            style: TextStyle(
+              color: Palette.secondary,
+              fontFamily: 'Inter',
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
         ),
       ),
     );
--- lib/theme/palette.dart
+++ lib/theme/palette.dart
@@ -2,4 +2,26 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static const primary = Color(0xFF58CC02);
+  static const primaryButtonStroke = Color(0xFF58A700);
+  static const primaryShadow = Color(0xFF58A700);
+  static const primaryText = Color(0xFF4A4A4A);
+  static const primaryFill = Color(0xFFD7FFB8);
+
+  static const primaryVariant = Color(0xFF1CB0F6);
+  static const primaryVariantStroke = Color(0xFF1899D6);
+  static const primaryVariantShadow = Color(0xFF1899D6);
+  static const primaryVariantText = Color(0xFF1899D6);
+
+  static const secondary = Color(0xFFFFFFFF);
+  static const secondaryStroke = Color(0xFFE5E5E5);
+  static const secondaryVariant = Color(0xFFDDF4FF);
+  static const secondaryVariantStroke = Color(0xFF84D8FF);
+  static const secondaryText = Color(0xFFAFAFAF);
+
+  static const tertiary = Color(0xFFFFC800);
+  static const onTertiary = Color(0xFFFFE279);
+
+  static const error = Color(0xFFFD4D50);
+  static const errorShadow = Color(0xFFD63E36);
+  static const errorFill = Color(0xFFFEC1C2);
 }
