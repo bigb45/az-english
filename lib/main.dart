e4cbbaaadffe12c0602f5fdb219bb2e557b6562e
ribhy-bishtawi <ribhy2001@gmail.com>
2024-04-15 18:05:17 +0300

    service: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:ez_english/firebase_options.dart';
 import 'package:ez_english/theme/palette.dart';
 import 'package:ez_english/widgets/button.dart';
 import 'package:ez_english/widgets/card.dart';
 import 'package:ez_english/widgets/result_card.dart';
 import 'package:ez_english/widgets/text_field.dart';
 import 'package:ez_english/widgets/word_chip.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
