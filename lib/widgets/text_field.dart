49eaa23cf822af240cb41ada55113e721cb0b2a7
Mohammed Natour <bgd4500@gmail.com>
2024-04-23 13:58:49 +0300

    refactor: Pass in the required parameters

--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -1,12 +1,13 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:ez_english/theme/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomTextField extends StatefulWidget {
-  // TODO: Pass in the required parameters (controller, focusNode, etc.)
-  const CustomTextField({super.key});
+  final TextEditingController controller;
+  final FocusNode? focusNode;
+  final String? hintText;
+  const CustomTextField(
+      {super.key, required this.controller, this.focusNode, this.hintText});
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -15,77 +16,54 @@ class CustomTextField extends StatefulWidget {
 class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
-    Color fillColor = Palette.textFieldFill;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextFormField(
-        enabled: true,
-        onTap: () {
-          setState(() {
-            fillColor = Palette.error;
-          });
-        },
-        onEditingComplete: () {
-          setState(() {
-            fillColor = Palette.textFieldFill;
-          });
-        },
-        style: TextStyle(
-          color: Palette.primaryText,
+    return TextFormField(
+      focusNode: widget.focusNode,
+      controller: widget.controller,
+      enabled: true,
+      style: TextStyle(
+        color: Palette.primaryText,
+        fontFamily: 'Inter',
+        fontSize: 14.sp,
+        fontWeight: FontWeight.w700,
+      ),
+      decoration: InputDecoration(
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.r),
+          borderSide: const BorderSide(
+            width: 2,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.r),
+          borderSide: const BorderSide(
+            color: Palette.secondaryStroke,
+            width: 2,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.r),
+          borderSide: const BorderSide(
+            color: Palette.error,
+            width: 2,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.r),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.r),
+          borderSide: const BorderSide(
+            color: Palette.secondaryVariantStroke,
+            width: 2,
+          ),
+        ),
+        hintStyle: TextStyle(
+          color: Palette.secondaryText,
           fontFamily: 'Inter',
           fontSize: 14.sp,
           fontWeight: FontWeight.w700,
         ),
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: fillColor,
-          disabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16.r),
-            borderSide: BorderSide(
-              color: fillColor,
-              width: 2,
-            ),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16.r),
-            borderSide: BorderSide(
-              color: Palette.secondaryStroke,
-              width: 2,
-            ),
-          ),
-
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16.r),
-            borderSide: BorderSide(
-              color: Palette.error,
-              width: 2,
-            ),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16.r),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16.r),
-            borderSide: BorderSide(
-              color: Palette.secondaryVariantStroke,
-              width: 2,
-            ),
-          ),
-          hintStyle: TextStyle(
-            color: Palette.secondaryText,
-            fontFamily: 'Inter',
-            fontSize: 14.sp,
-            fontWeight: FontWeight.w700,
-          ),
-          // helperText: "HELPER TEXT",
-          // helperStyle: TextStyle(
-          //   color: Palette.error,
-          //   fontFamily: 'Inter',
-          //   fontSize: 12,
-          //   fontWeight: FontWeight.w700,
-          // ),
-          hintText: 'Place Holder',
-        ),
+        hintText: widget.hintText ?? "Enter text",
       ),
     );
   }
