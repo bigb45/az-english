094ef2b9640cc36a0e683c445ad74ca472f169dd
Mohammed Natour <bgd4500@gmail.com>
2024-04-23 18:33:25 +0300

    feature: add Radio button group

---
+++ lib/widgets/radio_button.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class RadioGroup extends StatefulWidget {
+  final Function(RadioItemData) onChanged;
+  final List<RadioItemData> options;
+  final RadioItemData defaultOption;
+  const RadioGroup(
+      {super.key,
+      required this.onChanged,
+      required this.options,
+      required this.defaultOption});
+
+  @override
+  State<RadioGroup> createState() => _RadioGroupState();
+}
+
+class _RadioGroupState extends State<RadioGroup> {
+  RadioItemData? selectedOption;
+  @override
+  void initState() {
+    selectedOption = widget.options.isNotEmpty ? widget.options.first : null;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: List.generate(
+        widget.options.length,
+        (index) {
+          RadioItemData option = widget.options[index];
+          return ListTile(
+            title: Text(option.title),
+            leading: Radio<RadioItemData>(
+              value: option,
+              groupValue: selectedOption,
+              onChanged: (value) {
+                if (value != null) {
+                  widget.onChanged(value);
+                  setState(() {
+                    selectedOption = value;
+                  });
+                }
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+Widget RadioOption(String value, String title, Function(String) onChanged,
+    String selectedOption) {
+  return ListTile(
+    title: Text(title),
+    leading: Radio<String>(
+      value: value,
+      groupValue: selectedOption,
+      onChanged: onChanged(value),
+    ),
+  );
+}
+
+class RadioItemData {
+  final String title;
+  final String value;
+  RadioItemData({required this.title, required this.value});
+}
