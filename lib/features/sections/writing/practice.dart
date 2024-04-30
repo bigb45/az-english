a1e23798556fc7341b91d18df3b30ebbe720ae27
Mohammed Natour <bgd4500@gmail.com>
2024-04-30 22:40:12 +0300

    refactor: use styles from TextStyles

--- lib/features/sections/writing/practice.dart
+++ lib/features/sections/writing/practice.dart
@@ -1,13 +1,19 @@
 import 'dart:math';
 
 import 'package:ez_english/core/constants.dart';
+import 'package:ez_english/features/sections/components/evaluation_section.dart';
+import 'package:ez_english/features/sections/grammar/practice.dart';
 import 'package:ez_english/features/sections/writing/components/multiple_choice_question.dart';
 import 'package:ez_english/theme/palette.dart';
+import 'package:ez_english/theme/text_styles.dart';
 import 'package:ez_english/widgets/button.dart';
+import 'package:ez_english/widgets/exercise_card.dart';
 import 'package:ez_english/widgets/progress_bar.dart';
 import 'package:ez_english/widgets/radio_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
 class WritingPractice extends StatefulWidget {
@@ -23,6 +29,9 @@ class _WritingPracticeState extends State<WritingPractice> {
   @override
   void initState() {
     Future<void> configureTts() async {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        FlutterStatusbarcolor.setStatusBarColor(Colors.white);
+      });
       // flutterTts.setProgressHandler((_, __, ___, currentWord) {
       //   print("$currentWord");
       // });
@@ -63,27 +72,20 @@ class _WritingPracticeState extends State<WritingPractice> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 90.h,
-        backgroundColor: Palette.secondary,
-        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        backgroundColor: Colors.white,
         title: ListTile(
           contentPadding: const EdgeInsets.only(left: 0, right: 0),
           title: Text(
             'Writing & Listening Practice',
-            style: TextStyle(
-              fontSize: 24.sp,
+            style: TextStyles.titleTextStyle.copyWith(
               color: Palette.primaryText,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w600,
             ),
           ),
           subtitle: Text(
             "Daily Conversations",
-            style: TextStyle(
-              fontSize: 17.sp,
+            style: TextStyles.subtitleTextStyle.copyWith(
               color: Palette.primaryText,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w400,
             ),
           ),
         ),
@@ -124,64 +126,13 @@ class _WritingPracticeState extends State<WritingPractice> {
               ),
             ),
           ),
-          EvaluateAnswer()
+          EvaluateAnswer(
+            onPressed: () {
+              print("Continuing");
+            },
+          )
         ],
       ),
     );
   }
-}
-
-class EvaluateAnswer extends StatefulWidget {
-  const EvaluateAnswer({super.key});
-
-  @override
-  State<EvaluateAnswer> createState() => _EvaluateAnswerState();
-}
-
-class _EvaluateAnswerState extends State<EvaluateAnswer> {
-  EvaluationState evaulationState = EvaluationState.empty;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      color: switch (evaulationState) {
-        EvaluationState.correct => Palette.primaryFill,
-        EvaluationState.incorrect => Palette.errorFill,
-        _ => Palette.secondary,
-      },
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-            vertical: Constants.padding8, horizontal: Constants.padding8),
-        child: Button(
-          onPressed: () {
-            setState(() {
-              evaulationState = Random().nextBool()
-                  ? EvaluationState.correct
-                  : EvaluationState.incorrect;
-            });
-          },
-          type: switch (evaulationState) {
-            EvaluationState.correct => ButtonType.primary,
-            EvaluationState.incorrect => ButtonType.error,
-            _ => ButtonType.primaryVariant,
-          },
-          child: Text(
-            "CHECK",
-            style: TextStyle(
-              color: Palette.secondary,
-              fontFamily: 'Inter',
-              fontSize: 14.sp,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-enum EvaluationState {
-  correct,
-  incorrect,
-  empty,
 }
