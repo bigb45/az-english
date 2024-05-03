de42af74039cc67c3a85f57c91f0770066e6ba1e
Mohammed Natour <bgd4500@gmail.com>
2024-05-03 15:47:09 +0300

    feature: add evaluation functionality to the grammar section

--- lib/features/sections/grammar/components/sentence_forming_question.dart
+++ lib/features/sections/grammar/components/sentence_forming_question.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sized_box_for_whitespace
-
 import 'package:ez_english/core/Constants.dart';
 import 'package:ez_english/theme/palette.dart';
 import 'package:ez_english/theme/text_styles.dart';
@@ -8,12 +6,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SentenceFormingQuestion extends StatefulWidget {
-  // TODO: fix divider and wrap alignment
-  // TODO: make the words draggable, maybe not
   final String fullSentence;
   final String words;
+  final Function(String) onChanged;
   const SentenceFormingQuestion(
-      {super.key, required this.fullSentence, required this.words});
+      {super.key,
+      required this.fullSentence,
+      required this.words,
+      required this.onChanged});
 
   @override
   State<SentenceFormingQuestion> createState() =>
@@ -49,7 +49,7 @@ class _SentenceFormingQuestionState extends State<SentenceFormingQuestion> {
                     top: 40.w,
                     left: 0,
                     right: 0,
-                    child: Divider(
+                    child: const Divider(
                       thickness: 2,
                       color: Palette.secondaryStroke,
                     ),
@@ -58,7 +58,7 @@ class _SentenceFormingQuestionState extends State<SentenceFormingQuestion> {
                     top: 100.w,
                     left: 0,
                     right: 0,
-                    child: Divider(
+                    child: const Divider(
                       thickness: 2,
                       color: Palette.secondaryStroke,
                     ),
@@ -67,7 +67,7 @@ class _SentenceFormingQuestionState extends State<SentenceFormingQuestion> {
                     top: 160.w,
                     left: 0,
                     right: 0,
-                    child: Divider(
+                    child: const Divider(
                       thickness: 2,
                       color: Palette.secondaryStroke,
                     ),
@@ -89,6 +89,11 @@ class _SentenceFormingQuestionState extends State<SentenceFormingQuestion> {
                                     setState(() {
                                       orderedWords.remove(word);
                                       word.isSelected = false;
+                                      widget.onChanged(
+                                        orderedWords
+                                            .map((e) => e.text)
+                                            .join(" "),
+                                      );
                                     });
                                   },
                                   text: word.text,
@@ -112,6 +117,9 @@ class _SentenceFormingQuestionState extends State<SentenceFormingQuestion> {
             setState(() {
               orderedWords.add(value);
               value.isSelected = true;
+              widget.onChanged(
+                orderedWords.map((e) => e.text).join(" "),
+              );
             });
             print(
               orderedWords.map((e) => e.text).join(
--- lib/features/sections/grammar/practice.dart
+++ lib/features/sections/grammar/practice.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:ez_english/core/constants.dart';
 import 'package:ez_english/features/sections/components/evaluation_section.dart';
-import 'package:ez_english/features/sections/grammar/components/drag_and_drop_question.dart';
+import 'package:ez_english/features/sections/grammar/components/sentence_forming_question.dart';
 import 'package:ez_english/theme/palette.dart';
 import 'package:ez_english/theme/text_styles.dart';
 import 'package:ez_english/widgets/button.dart';
@@ -15,13 +15,21 @@ import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
 class GrammarPractice extends StatefulWidget {
-  const GrammarPractice({super.key});
+  // TODO: make sure divider is aligned properly
+
+  // TODO:
+  final String fullSentence;
+  final String options;
+  const GrammarPractice(
+      {super.key, required this.fullSentence, required this.options});
 
   @override
   State<GrammarPractice> createState() => _GrammarPracticeState();
 }
 
 class _GrammarPracticeState extends State<GrammarPractice> {
+  String userAnswer = "";
+
   @override
   void initState() {
     super.initState();
@@ -53,16 +61,21 @@ class _GrammarPracticeState extends State<GrammarPractice> {
       ),
       body: Column(
         children: [
-          const Expanded(
+          Expanded(
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: SingleChildScrollView(
                 child: Column(
                   children: [
+                    const ProgressBar(value: 20),
                     SentenceFormingQuestion(
-                      fullSentence: "The dog jumps over the fence",
-                      words:
-                          "The jumps dog fence dogs jumped over under the jumped over under the",
+                      onChanged: (value) {
+                        setState(() {
+                          userAnswer = value;
+                        });
+                      },
+                      fullSentence: widget.fullSentence,
+                      words: widget.options,
                     )
                   ],
                 ),
@@ -72,7 +85,12 @@ class _GrammarPracticeState extends State<GrammarPractice> {
           EvaluateAnswer(
             state: answerState,
             onPressed: () {
-              print("Continuing");
+              setState(() {
+                answerState = widget.fullSentence.toLowerCase() ==
+                        userAnswer.toLowerCase()
+                    ? EvaluationState.correct
+                    : EvaluationState.incorrect;
+              });
             },
           )
         ],
