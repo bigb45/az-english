b0a59eb3c7601d13a91a5c6506dbc99963267820
ribhy-bishtawi <ribhy2001@gmail.com>
2024-05-10 22:59:27 +0300

    feature: add multiple select question component for reading section

---
+++ lib/features/sections/reading/components/multiple-select question.dart
@@ -0,0 +1,82 @@
+import 'package:ez_english/core/constants.dart';
+import 'package:ez_english/resources/app_strings.dart';
+import 'package:ez_english/theme/palette.dart';
+import 'package:ez_english/theme/text_styles.dart';
+import 'package:ez_english/widgets/button.dart';
+import 'package:ez_english/widgets/checkbox.dart';
+import 'package:ez_english/widgets/expandable_text.dart';
+import 'package:ez_english/widgets/microphone_button.dart';
+import 'package:ez_english/widgets/progress_bar.dart';
+import 'package:ez_english/widgets/radio_button.dart';
+import 'package:ez_english/widgets/text_box.dart';
+import 'package:ez_english/widgets/text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+class MultipleSelectQuestion extends StatefulWidget {
+  const MultipleSelectQuestion({
+    super.key,
+  });
+
+  @override
+  State<MultipleSelectQuestion> createState() => _MultipleSelectQuestionState();
+}
+
+class _MultipleSelectQuestionState extends State<MultipleSelectQuestion> {
+  bool _isFocused = false;
+  bool _isReadMore = false;
+  String readMoreText = AppStrings.mcQuestionReadMoreText;
+// TODO dynamic list
+  var optList = [
+    CheckboxData(
+      title: "Option 1",
+    ),
+    CheckboxData(
+      title: "Option 2",
+    ),
+    CheckboxData(
+      title: "Option 3",
+    ),
+    CheckboxData(
+      title: "Option 4",
+    ),
+  ];
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  TextEditingController controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Constants.gapH24,
+        ExpandableTextBox(
+            // TODO dynamic text
+            questionText:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+            isFocused: _isFocused,
+            isReadMore: _isReadMore,
+            readMoreText: readMoreText),
+        Constants.gapH24,
+        // TODO: Is it a fixed question or should it be dynamic?
+        Text(
+          AppStrings.mcQuestionText,
+          style: TextStyles.questionTextStyle,
+        ),
+        Constants.gapH24,
+        CheckboxGroup(
+            onChanged: (selections) {
+              List<bool?> options = selections.map((e) => e.value).toList();
+              print("new selections: $options");
+              setState(() {
+                _isFocused = false;
+              });
+            },
+            options: optList),
+      ],
+    );
+  }
+}
