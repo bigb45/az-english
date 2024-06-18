152d6623f2b33a7bc6d4e3f49c96ff891d501b20
Mohammed Natour <bgd4500@gmail.com>
2024-06-18 18:10:00 +0300

    feature: create general buildQuestionWidget method for all question types

--- lib/features/sections/reading/practice.dart
+++ lib/features/sections/reading/practice.dart
@@ -1,13 +1,21 @@
+import 'package:ez_english/core/constants.dart';
 import 'package:ez_english/features/models/base_question.dart';
 import 'package:ez_english/features/sections/components/evaluation_section.dart';
 import 'package:ez_english/features/sections/components/leave_alert_dialog.dart';
+import 'package:ez_english/features/sections/grammar/components/sentence_forming_question.dart';
+import 'package:ez_english/features/sections/models/dictation_question_model.dart';
+import 'package:ez_english/features/sections/models/reading_question_model.dart';
+import 'package:ez_english/features/sections/models/sentence_forming_question_model.dart';
+import 'package:ez_english/features/sections/models/speaking_question_model.dart';
+import 'package:ez_english/features/sections/models/youtube_lesson_model.dart';
 import 'package:ez_english/features/sections/reading/components/multiple-choice%20question.dart';
 import 'package:ez_english/features/sections/reading/components/speaking_question.dart';
-import 'package:ez_english/features/sections/reading/model/reading_question.dart';
 import 'package:ez_english/features/sections/reading/view_model/reading_section_viewmodel.dart';
+import 'package:ez_english/features/sections/writing/components/dictation_question.dart';
 import 'package:ez_english/resources/app_strings.dart';
 import 'package:ez_english/theme/palette.dart';
 import 'package:ez_english/widgets/progress_bar.dart';
+import 'package:ez_english/youtube.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -23,7 +31,6 @@ class ReadingPractice extends StatefulWidget {
 class _ReadingPracticeState extends State<ReadingPractice> {
   late ReadingQuestionViewmodel readingSectionViewmodel;
   late List<BaseQuestion> questions = [];
-  int currentQuestionIndex = 0;
 
   @override
   void initState() {
@@ -34,18 +41,17 @@ class _ReadingPracticeState extends State<ReadingPractice> {
     super.initState();
   }
 
-  EvaluationState answerState = EvaluationState.empty;
   @override
   Widget build(BuildContext context) {
     ReadingQuestionModel currentQuestion =
-        questions[currentQuestionIndex] as ReadingQuestionModel;
+        questions[readingSectionViewmodel.currentIndex] as ReadingQuestionModel;
 
     return PopScope(
       canPop: false,
       onPopInvoked: (canPop) {
         showLeaveAlertDialog(context, onConfirm: () async {
           await readingSectionViewmodel
-              .updateSectionProgress(currentQuestionIndex);
+              .updateSectionProgress(readingSectionViewmodel.currentIndex);
         });
       },
       child: Scaffold(
@@ -61,8 +67,8 @@ class _ReadingPracticeState extends State<ReadingPractice> {
                 showLeaveAlertDialog(
                   context,
                   onConfirm: () async {
-                    await readingSectionViewmodel
-                        .updateSectionProgress(currentQuestionIndex);
+                    await readingSectionViewmodel.updateSectionProgress(
+                        readingSectionViewmodel.currentIndex);
                   },
                 );
               },
@@ -81,7 +87,7 @@ class _ReadingPracticeState extends State<ReadingPractice> {
                 fontWeight: FontWeight.w600,
               ),
             ),
-            // TODO change it to dymaic string from the API
+            // TODO change subtitle to dymaic string from the API
             subtitle: Text(
               "Daily Conversations",
               style: TextStyle(
@@ -97,28 +103,34 @@ class _ReadingPracticeState extends State<ReadingPractice> {
           children: [
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                padding: EdgeInsets.symmetric(horizontal: Constants.padding8),
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 8.0),
-                        child: ProgressBar(value: 20),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: Constants.padding8),
+                        child: const ProgressBar(value: 20),
                       ),
-                      buildQuestionWidget(currentQuestion),
+                      buildQuestionWidget(
+                        question: currentQuestion,
+                        onChanged: (value) {
+                          readingSectionViewmodel.updateAnswer(value);
+                        },
+                        answerState: readingSectionViewmodel.answerState,
+                      )
                     ],
                   ),
                 ),
               ),
             ),
             EvaluationSection(
+              onContinue: () {
+                readingSectionViewmodel.incrementIndex();
+              },
               onPressed: () {
                 setState(() {
-                  // TODO: check the answer and update the state accordingly
-                  currentQuestionIndex++;
-                  // evaulationState = Random().nextBool()
-                  //     ? EvaluationState.correct
-                  //     : EvaluationState.incorrect;
+                  readingSectionViewmodel.evaluateAnswer();
                 });
               },
             )
@@ -127,64 +139,47 @@ class _ReadingPracticeState extends State<ReadingPractice> {
       ),
     );
   }
-
-  Widget buildQuestionWidget(ReadingQuestionModel question) {
-    switch (question.questionType) {
-      case QuestionType.speaking:
-        return const SpeakingQuestion();
-      case QuestionType.multipleChoice:
-        return const MultipleChoiceQuestion();
-      // Add more cases for other question types if needed
-      default:
-        return const SizedBox(); // Default case
-    }
-  }
 }
 
-// TODO: make sure this is unused & remove it
+// TODO: move this to separate file
+Widget buildQuestionWidget(
+    {required BaseQuestion question,
+    required Function(String) onChanged,
+    required EvaluationState answerState}) {
+  switch (question.questionType) {
+    case QuestionType.speaking:
+      return SpeakingQuestion(
+        question: question as SpeakingQuestionModel,
+      );
 
-// class EvaluateAnswer extends StatefulWidget {
-//   const EvaluateAnswer({super.key});
+    case QuestionType.sentenceForming:
+      return SentenceFormingQuestion(
+        question: question as SentenceFormingQuestionModel,
+        onChanged: onChanged,
+        answerState: answerState,
+      );
 
-//   @override
-//   State<EvaluateAnswer> createState() => _EvaluateAnswerState();
-// }
+    case QuestionType.multipleChoice:
+      return const MultipleChoiceQuestion();
 
-// class _EvaluateAnswerState extends State<EvaluateAnswer> {
-//   EvaluationState evaulationState = EvaluationState.empty;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: double.infinity,
-//       color: switch (evaulationState) {
-//         EvaluationState.correct => Palette.primaryFill,
-//         EvaluationState.incorrect => Palette.errorFill,
-//         _ => Palette.secondary,
-//       },
-//       child: Padding(
-//         padding: EdgeInsets.symmetric(
-//             vertical: Constants.padding8, horizontal: Constants.padding8),
-//         child: Button(
-//             onPressed: () {
-//               setState(() {
-//                 evaulationState = Random().nextBool()
-//                     ? EvaluationState.correct
-//                     : EvaluationState.incorrect;
-//               });
-//             },
-//             type: switch (evaulationState) {
-//               EvaluationState.correct => ButtonType.primary,
-//               EvaluationState.incorrect => ButtonType.error,
-//               _ => ButtonType.primaryVariant,
-//             },
-//             text: "check"),
-//       ),
-//     );
-//   }
-// }
+    case QuestionType.dictation:
+      return DictationQuestion(
+        onAnswerChanged: onChanged,
+        question: question as DictationQuestionModel,
+      );
 
-// enum EvaluationState {
-//   correct,
-//   incorrect,
-//   empty,
-// }
+    case QuestionType.findWordsFromPassage:
+      return const Text("Find Words From Passage Question");
+
+    case QuestionType.answerQuestionsFromPassage:
+      return const Text("Answer Questions From Passage Question");
+
+    case QuestionType.youtubeLesson:
+      return YouTubeVideoPlayer(
+        videoId: (question as YoutubeLessonModel).youtubeUrl!,
+      );
+
+    default:
+      return const Text("Unsupported Question Type");
+  }
+}
