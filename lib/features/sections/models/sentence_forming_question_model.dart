955d3cde6f5191c475e3244ed4ecd3b2d4898c8b
Mohammed Natour <bgd4500@gmail.com>
2024-06-18 18:08:09 +0300

    feature: create sentence forming question model

---
+++ lib/features/sections/models/sentence_forming_question_model.dart
@@ -0,0 +1,20 @@
+import 'package:ez_english/features/models/base_question.dart';
+
+class SentenceFormingQuestionModel extends BaseQuestion<String> {
+  final String words;
+  final String correctAnswer;
+  final String question;
+  // TODO: figure out how to make the same question incorporate partial sentences
+  final String? partialSentence = null;
+  SentenceFormingQuestionModel({
+    required this.question,
+    required this.words,
+    required this.correctAnswer,
+  }) : super(
+            questionTextInEnglish: question,
+            questionTextInArabic: question,
+            imageUrl: '',
+            voiceUrl: '',
+            questionType: QuestionType.sentenceForming,
+            answer: correctAnswer);
+}
