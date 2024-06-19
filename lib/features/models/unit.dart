bfd68e638af311c75d6f7aad07df7332eaf57bfa
ribhy-bishtawi <ribhy2001@gmail.com>
2024-06-20 00:41:16 +0300

    refactor: add description fields to Unit model

--- lib/features/models/unit.dart
+++ lib/features/models/unit.dart
@@ -4,12 +4,22 @@ import 'package:ez_english/features/models/base_question.dart';
 
 class Unit {
   String name;
+  String? descriptionInEnglish;
+  String? descriptionInArabic;
   List<BaseQuestion> questions;
 
-  Unit({required this.name, required this.questions});
+  Unit(
+      {required this.name,
+      required this.descriptionInEnglish,
+      required this.descriptionInArabic,
+      required this.questions});
   factory Unit.fromMap(Map<String, dynamic> map) {
     return Unit(
       name: map['name'] ?? '',
+      descriptionInEnglish:
+          map['descriptionInEnglish'] ?? 'No English description',
+      descriptionInArabic:
+          map['descriptionInArabic'] ?? "No Arabic Description",
       questions: (map['questions'] as List)
           .map((item) => BaseQuestion.fromMap(item))
           .toList(),
@@ -19,6 +29,8 @@ class Unit {
   Map<String, dynamic> toMap() {
     return {
       'name': name,
+      'descriptionInEnglish': descriptionInEnglish,
+      'descriptionInArabic': descriptionInArabic,
       'questions': questions.map((q) => q.toMap()).toList(),
     };
   }
