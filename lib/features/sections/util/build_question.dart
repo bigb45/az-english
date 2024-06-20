01309ba798d0bec26bf46cdee756d4db447a33ac
Mohammed Natour <bgd4500@gmail.com>
2024-06-20 10:56:01 +0300

    refactor: make buildQuestion function accept generic parameters

--- lib/features/sections/util/build_question.dart
+++ lib/features/sections/util/build_question.dart
@@ -1,19 +1,20 @@
 import 'package:ez_english/features/models/base_question.dart';
 import 'package:ez_english/features/sections/components/dictation_question.dart';
 import 'package:ez_english/features/sections/components/evaluation_section.dart';
-import 'package:ez_english/features/sections/components/multiple_choice_question.dart';
+import 'package:ez_english/features/sections/components/generic_multiple_choice_question.dart';
 import 'package:ez_english/features/sections/components/sentence_forming_question.dart';
 import 'package:ez_english/features/sections/components/speaking_question.dart';
 import 'package:ez_english/features/sections/components/youtube_lesson.dart';
 import 'package:ez_english/features/sections/models/dictation_question_model.dart';
+import 'package:ez_english/features/sections/models/multiple_choice_question_model.dart';
 import 'package:ez_english/features/sections/models/sentence_forming_question_model.dart';
 import 'package:ez_english/features/sections/models/speaking_question_model.dart';
 import 'package:ez_english/features/sections/models/youtube_lesson_model.dart';
 import 'package:flutter/material.dart';
 
-Widget buildQuestion(
+Widget buildQuestion<T>(
     {required BaseQuestion question,
-    required Function(String) onChanged,
+    required Function(T) onChanged,
     required EvaluationState answerState}) {
   switch (question.questionType) {
     case QuestionType.speaking:
@@ -24,16 +25,19 @@ Widget buildQuestion(
     case QuestionType.sentenceForming:
       return SentenceFormingQuestion(
         question: question as SentenceFormingQuestionModel,
-        onChanged: onChanged,
+        onChanged: (value) => onChanged(value as T),
         answerState: answerState,
       );
 
     case QuestionType.multipleChoice:
-      return const MultipleChoiceQuestion();
+      return GenericMultipleChoiceQuestion(
+        question: question as MultipleChoiceQuestionModel,
+        onChanged: (value) => onChanged(value as T),
+      );
 
     case QuestionType.dictation:
       return DictationQuestion(
-        onAnswerChanged: onChanged,
+        onAnswerChanged: (value) => onChanged(value as T),
         question: question as DictationQuestionModel,
       );
 
