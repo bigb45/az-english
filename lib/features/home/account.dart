bc35d836a182eff0a94dc1b9605325ad979d76a9
ribhy-bishtawi <ribhy2001@gmail.com>
2024-06-22 19:29:41 +0300

    refactor: optimize account page layout

--- lib/features/home/account.dart
+++ lib/features/home/account.dart
@@ -22,73 +22,75 @@ class Account extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Text(
-            //   'Signed in as ${vm.user?.email}',
-            //   style: TextStyles.bodyLarge,
-            // ),
-            const Icon(
-              Icons.account_circle,
-              size: 200,
-              color: Palette.primaryText,
-            ),
-            InfoCard(AppStrings.username, "${vm.user?.displayName}"),
-            InfoCard(AppStrings.emailAddress, "${vm.user?.email}"),
-            InfoCard(AppStrings.phoneNumber, "${vm.user?.phoneNumber}"),
-            // Padding(
-            //   padding: EdgeInsets.all(Constants.padding12),
-            //   child: Card(
-            //     elevation: 0,
-            //     child: Padding(
-            //       padding: EdgeInsets.all(Constants.padding12),
-            //       child: SizedBox(
-            //         width: double.infinity,
-            //         child: Column(
-            //           mainAxisAlignment: MainAxisAlignment.start,
-            //           crossAxisAlignment: CrossAxisAlignment.stretch,
-            //           children: [
-            //             Text(
-            //               'User ID',
-            //               style: TextStyles.bodyLarge,
-            //             ),
-            //             Text(
-            //               " ${vm.user?.uid}",
-            //               style: TextStyles.bodyMedium,
-            //             )
-            //           ],
-            //         ),
-            //       ),
-            //     ),
-            //   ),
-            // ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Text(
+              //   'Signed in as ${vm.user?.email}',
+              //   style: TextStyles.bodyLarge,
+              // ),
+              const Icon(
+                Icons.account_circle,
+                size: 200,
+                color: Palette.primaryText,
+              ),
+              InfoCard(AppStrings.username, "${vm.user?.displayName}"),
+              InfoCard(AppStrings.emailAddress, "${vm.user?.email}"),
+              InfoCard(AppStrings.phoneNumber, "${vm.user?.phoneNumber}"),
+              // Padding(
+              //   padding: EdgeInsets.all(Constants.padding12),
+              //   child: Card(
+              //     elevation: 0,
+              //     child: Padding(
+              //       padding: EdgeInsets.all(Constants.padding12),
+              //       child: SizedBox(
+              //         width: double.infinity,
+              //         child: Column(
+              //           mainAxisAlignment: MainAxisAlignment.start,
+              //           crossAxisAlignment: CrossAxisAlignment.stretch,
+              //           children: [
+              //             Text(
+              //               'User ID',
+              //               style: TextStyles.bodyLarge,
+              //             ),
+              //             Text(
+              //               " ${vm.user?.uid}",
+              //               style: TextStyles.bodyMedium,
+              //             )
+              //           ],
+              //         ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
 
-            Button(
-              text: 'Sign out',
-              onPressed: () async {
-                await vm.signOut(context);
-              },
-            ),
-            // SizedBox(
-            //   height: Constants.padding8,
-            // ),
-            // Button(
-            //   text: 'Go to Components',
-            //   onPressed: () {
-            //     context.push('/components');
-            //   },
-            // ),
-            // SizedBox(
-            //   height: Constants.padding8,
-            // ),
-            // Button(
-            //   text: 'Go to Settings',
-            //   onPressed: () {
-            //     context.push('/settings');
-            //   },
-            // ),
-          ],
+              Button(
+                text: 'Sign out',
+                onPressed: () async {
+                  await vm.signOut(context);
+                },
+              ),
+              // SizedBox(
+              //   height: Constants.padding8,
+              // ),
+              // Button(
+              //   text: 'Go to Components',
+              //   onPressed: () {
+              //     context.push('/components');
+              //   },
+              // ),
+              // SizedBox(
+              //   height: Constants.padding8,
+              // ),
+              // Button(
+              //   text: 'Go to Settings',
+              //   onPressed: () {
+              //     context.push('/settings');
+              //   },
+              // ),
+            ],
+          ),
         ),
       ),
     );
