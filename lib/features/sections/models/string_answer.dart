219bc4306624bfb2661903b7f204ab4f305157b1
ribhy-bishtawi <ribhy2001@gmail.com>
2024-06-23 13:34:57 +0300

    refactor: add generic toMap method in base answer class

--- lib/features/sections/models/string_answer.dart
+++ lib/features/sections/models/string_answer.dart
@@ -3,21 +3,12 @@ import 'package:ez_english/features/models/base_answer.dart';
 class StringAnswer extends BaseAnswer<String> {
   StringAnswer({
     String? answer,
-  }) : super(
-          answerType: AnswerType.string,
-        );
+  }) : super(answerType: AnswerType.string, answer: answer);
 
   @override
   bool validate(BaseAnswer? userAnswer) {
     // TODO: compare answers here and return true or false
     // might change return type to AnswerValidationResult
     return false;
-  }
-
-  @override
-  Map<String, dynamic> toMap() {
-    return {
-      'answer': answer,
-    };
   }
 }
