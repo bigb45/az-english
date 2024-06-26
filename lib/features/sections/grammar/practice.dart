463f683ee86a9ce09eb865ce5848eee96ad93635
Mohammed Natour <bgd4500@gmail.com>
2024-06-27 00:03:47 +0300

    feature: enable errors and progress bar in ui

--- lib/features/sections/grammar/practice.dart
+++ lib/features/sections/grammar/practice.dart
@@ -1,3 +1,4 @@
+import 'package:ez_english/core/constants.dart';
 import 'package:ez_english/features/models/base_question.dart';
 import 'package:ez_english/features/sections/components/evaluation_section.dart';
 import 'package:ez_english/features/sections/components/leave_alert_dialog.dart';
@@ -32,83 +33,106 @@ class _GrammarPracticeState extends State<GrammarPractice> {
 
   @override
   Widget build(BuildContext context) {
-    grammarSectionViewmodel = Provider.of<GrammarSectionViewmodel>(context);
+    return Consumer<GrammarSectionViewmodel>(
+        builder: (context, grammarSectionViewmodel, _) {
+      BaseQuestion currentQuestion =
+          questions[grammarSectionViewmodel.currentIndex];
 
-    BaseQuestion currentQuestion =
-        questions[grammarSectionViewmodel.currentIndex];
-
-    return PopScope(
-      canPop: false,
-      onPopInvoked: (canPop) {
-        showLeaveAlertDialog(context);
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              icon: const Icon(
-                Icons.close,
-                color: Palette.primaryText,
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (grammarSectionViewmodel.error != null) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(
+                SnackBar(
+                  content: Text(
+                    grammarSectionViewmodel.error?.message ??
+                        "An error occurred",
+                  ),
+                ),
+              )
+              .closed
+              .then((reason) => grammarSectionViewmodel.resetError());
+        }
+      });
+      return PopScope(
+        canPop: false,
+        onPopInvoked: (canPop) {
+          showLeaveAlertDialog(context);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            actions: [
+              IconButton(
+                icon: const Icon(
+                  Icons.close,
+                  color: Palette.primaryText,
+                ),
+                onPressed: () {
+                  showLeaveAlertDialog(
+                    context,
+                  );
+                },
               ),
-              onPressed: () {
-                showLeaveAlertDialog(
-                  context,
-                );
-              },
-            ),
-          ],
-          systemOverlayStyle: SystemUiOverlayStyle.dark,
-          backgroundColor: Colors.white,
-          title: ListTile(
-            contentPadding: const EdgeInsets.only(left: 0, right: 0),
-            // TODO: get title and subtitle from viewmodel
-            title: Text(
-              "Grammar Practice",
-              style: TextStyles.titleTextStyle
-                  .copyWith(color: Palette.primaryText),
-            ),
-            subtitle: Text(
-              "Daily Conversations",
-              style: TextStyles.subtitleTextStyle
-                  .copyWith(color: Palette.primaryText),
+            ],
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
+            backgroundColor: Colors.white,
+            title: ListTile(
+              contentPadding: const EdgeInsets.only(left: 0, right: 0),
+              // TODO: get title and subtitle from viewmodel
+              title: Text(
+                "Grammar Practice",
+                style: TextStyles.titleTextStyle
+                    .copyWith(color: Palette.primaryText),
+              ),
+              subtitle: Text(
+                "Daily Conversations",
+                style: TextStyles.subtitleTextStyle
+                    .copyWith(color: Palette.primaryText),
+              ),
             ),
           ),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      // TODO: change this to actual value (current question index / total questions)
-                      const ProgressBar(value: 20),
-
-                      Center(
-                        child: buildQuestion(
-                            question: currentQuestion,
-                            onChanged: (value) {
-                              grammarSectionViewmodel.updateAnswer(value);
-                            },
-                            answerState: grammarSectionViewmodel.answerState),
-                      ),
-                    ],
+          body: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: Constants.padding8,
+                          ),
+                          child: ProgressBar(
+                            value: grammarSectionViewmodel.currentIndex + 1,
+                            minValue: 0,
+                            maxValue: questions.length.toDouble(),
+                          ),
+                        ),
+                        Center(
+                          child: buildQuestion(
+                              question: currentQuestion,
+                              onChanged: (value) {
+                                grammarSectionViewmodel.updateAnswer(value);
+                              },
+                              answerState: grammarSectionViewmodel.answerState),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            EvaluationSection(
-              onContinue: () {
-                grammarSectionViewmodel.incrementIndex();
-              },
-              onPressed: grammarSectionViewmodel.evaluateAnswer,
-              state: grammarSectionViewmodel.answerState,
-            ),
-          ],
+              EvaluationSection(
+                onContinue: () {
+                  grammarSectionViewmodel.incrementIndex();
+                },
+                onPressed: grammarSectionViewmodel.evaluateAnswer,
+                state: grammarSectionViewmodel.answerState,
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
