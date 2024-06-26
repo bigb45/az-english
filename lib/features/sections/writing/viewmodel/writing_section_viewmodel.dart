d884c98f25c46b0847964e9a6043960114ed6a98
Mohammed Natour <bgd4500@gmail.com>
2024-06-26 18:28:27 +0300

    feature: fetch data from remote api

--- lib/features/sections/writing/viewmodel/writing_section_viewmodel.dart
+++ lib/features/sections/writing/viewmodel/writing_section_viewmodel.dart
@@ -9,44 +9,13 @@ import 'package:ez_english/features/models/base_question.dart';
 import 'package:ez_english/features/models/base_viewmodel.dart';
 import 'package:ez_english/features/models/user.dart';
 import 'package:ez_english/features/sections/components/evaluation_section.dart';
-import 'package:ez_english/features/sections/models/dictation_question_model.dart';
-import 'package:ez_english/features/sections/models/multiple_choice_answer.dart';
-import 'package:ez_english/features/sections/models/multiple_choice_question_model.dart';
-import 'package:ez_english/features/sections/models/string_answer.dart';
-import 'package:ez_english/widgets/radio_button.dart';
 
 class WritingSectionViewmodel extends BaseViewModel {
   final sectionId = "2";
 
   String? _levelName;
   String? levelId;
-  final List<BaseQuestion> _questions = [
-    MultipleChoiceQuestionModel(
-      questionTextInArabic: "What is the plural of 'goose'?",
-      questionTextInEnglish: "What is the plural of 'goose'?",
-      options: [
-        RadioItemData(title: "Geese", value: "geese"),
-        RadioItemData(title: "Gooses", value: "gooses"),
-        RadioItemData(title: "Geeses", value: "geeses"),
-        RadioItemData(title: "Gees", value: "gees"),
-      ],
-      // questionText: "What is the plural of 'goose'?",
-      imageUrl: "",
-      // voiceUrl: "",
-      answer: MultipleChoiceAnswer(
-        answer: RadioItemData(title: "Geese", value: "geese"),
-      ),
-    ),
-    DictationQuestionModel(
-      questionTextInArabic: "Dictate the following text",
-      questionTextInEnglish: "Dictate the following text",
-      voiceUrl: "",
-      speakableText: "The quick brown fox",
-      answer: StringAnswer(
-        answer: "The quick brown fox",
-      ),
-    ),
-  ];
+  List<BaseQuestion> _questions = [];
 
   // dynamic _userAnswer;
 
@@ -70,16 +39,19 @@ class WritingSectionViewmodel extends BaseViewModel {
     // int lastQuestionIndex = userData.levelsProgress![_levelName]!
     //     .sectionProgress![_sectionName]!.lastStoppedQuestionIndex;
     try {
-      // _questions =
-      //     await _firestoreService.fetchQuestions(_sectionName, _levelName!, 0);
+      var fetchedQuestions = await _firestoreService.fetchQuestions(
+        RouteConstants.writingSectionName,
+        _levelName!,
+        unitName: "Unit2",
+        0,
+      );
+      _questions = fetchedQuestions.cast<BaseQuestion>();
+
       error = null;
     } on CustomException catch (e) {
       error = e;
-
-      notifyListeners();
     } catch (e) {
-      // TODO: assign error to 'error' variable here and handle state in UI
-      // error = CustomException(e.toString());
+      error = CustomException(e.toString());
     } finally {
       isLoading = false;
       notifyListeners();
@@ -93,6 +65,7 @@ class WritingSectionViewmodel extends BaseViewModel {
 
   void evaluateAnswer() {
     if (_questions[currentIndex].evaluateAnswer()) {
+      // TODO: perform scoring logic here
       answerState = EvaluationState.correct;
     } else {
       answerState = EvaluationState.incorrect;
@@ -102,7 +75,7 @@ class WritingSectionViewmodel extends BaseViewModel {
   void incrementIndex() {
     if (currentIndex < _questions.length - 1) {
       currentIndex = currentIndex + 1;
-      notifyListeners();
+      answerState = EvaluationState.empty;
     }
   }
 }
