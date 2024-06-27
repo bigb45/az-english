ed6c2c9d436019108dfa5c88678c69e23aed9408
Mohammed Natour <bgd4500@gmail.com>
2024-06-27 18:36:07 +0300

    refactor: clean viewmodel and pass error to ui

--- lib/features/sections/vocabulary/viewmodel/vocabulary_section_viewmodel.dart
+++ lib/features/sections/vocabulary/viewmodel/vocabulary_section_viewmodel.dart
@@ -2,15 +2,8 @@ import 'dart:async';
 
 import 'package:ez_english/core/constants.dart';
 import 'package:ez_english/core/firebase/exceptions.dart';
-import 'package:ez_english/core/firebase/firebase_authentication_service.dart';
-import 'package:ez_english/core/firebase/firestore_service.dart';
-import 'package:ez_english/features/models/base_answer.dart';
 import 'package:ez_english/features/models/base_question.dart';
 import 'package:ez_english/features/models/base_viewmodel.dart';
-import 'package:ez_english/features/models/user.dart';
-import 'package:ez_english/features/sections/components/evaluation_section.dart';
-import 'package:ez_english/features/sections/models/word_definition.dart';
-import 'package:ez_english/utils/utils.dart';
 
 import '../../models/word_model.dart';
 
@@ -18,31 +11,20 @@ class VocabularySectionViewmodel extends BaseViewModel {
   late List<WordModel> _words;
   String? levelId;
   String? _levelName;
-  UserModel? _userData;
   String? get levelName => _levelName;
   List<BaseQuestion?> _questions = [];
 
   List<BaseQuestion?> get questions => _questions;
   get words => _words;
-  final FirestoreService _firestoreService = FirestoreService();
-  final FirebaseAuthService _firebaseAuthService = FirebaseAuthService();
 
   @override
   FutureOr<void> init() {
-    _words = const [
-      WordModel(word: "View", type: WordType.verb, isNew: false),
-      WordModel(word: "View", type: WordType.verb, isNew: false),
-      WordModel(word: "View", type: WordType.verb, isNew: false),
-      WordModel(word: "View", type: WordType.verb, isNew: false),
-      WordModel(word: "View", type: WordType.verb, isNew: false),
-      WordModel(word: "View", type: WordType.verb, isNew: false),
-    ];
+    _words = [];
   }
 
   void setValuesAndInit() async {
     currentIndex = 0;
     _levelName = RouteConstants.getLevelName(levelId!);
-    await getUserData(_firebaseAuthService.getUser()!.uid);
     fetchQuestions();
   }
 
@@ -59,25 +41,13 @@ class VocabularySectionViewmodel extends BaseViewModel {
 
       error = null;
     } on CustomException catch (e) {
-      // error = e as CustomException;
-      _handleError(e.message);
+      error = e;
       notifyListeners();
     } catch (e) {
-      // TODO: assign error to 'error' variable here and handle state in UI
-      // error = CustomException(e.toString());
-      _handleError("An undefined error occurred ${e.toString()}");
+      error = CustomException("An undefined error occurred $e");
     } finally {
       isLoading = false;
       notifyListeners();
     }
-  }
-
-  void _handleError(String e) {
-    // TODO: separate UI logic from business logic
-    Utils.showSnackBar(e);
-  }
-
-  Future<void> getUserData(String userId) async {
-    _userData = await _firestoreService.getUser(userId);
   }
 }
