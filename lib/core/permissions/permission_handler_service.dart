7627ef64ec99972d0db6dccbdd5dd92a6a25e859
ribhy-bishtawi <ribhy2001@gmail.com>
2024-07-15 16:51:56 +0300

    feat: add permission handler package configurations and methods

---
+++ lib/core/permissions/permission_handler_service.dart
@@ -0,0 +1,35 @@
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionHandlerService {
+  Future<bool> requestPhotoPermission() async {
+    var status = await Permission.photos.status;
+    if (status.isDenied) {
+      status = await Permission.photos.request();
+    }
+    return status.isGranted;
+  }
+
+  Future<bool> requestCameraPermission() async {
+    var status = await Permission.camera.status;
+    if (!status.isGranted) {
+      status = await Permission.camera.request();
+    }
+    return status.isGranted;
+  }
+
+  Future<bool> requestMicrophonePermission() async {
+    var status = await Permission.microphone.status;
+    if (!status.isGranted) {
+      status = await Permission.microphone.request();
+    }
+    return status.isGranted;
+  }
+
+  Future<bool> requestStoragePermission() async {
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      status = await Permission.storage.request();
+    }
+    return status.isGranted;
+  }
+}
