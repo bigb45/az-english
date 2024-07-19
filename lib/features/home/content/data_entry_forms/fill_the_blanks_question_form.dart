4ad448436408bb9e9ca20d5dbc7a6388156b06dd
Mohammed Natour <bgd4500@gmail.com>
2024-07-19 18:28:27 +0300

    refactor: redesign fill the blanks question data entry form

--- lib/features/home/content/data_entry_forms/fill_the_blanks_question_form.dart
+++ lib/features/home/content/data_entry_forms/fill_the_blanks_question_form.dart
@@ -1,9 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:ez_english/features/home/content/data_entry_forms/dictation_question_form.dart';
 import 'package:ez_english/features/home/content/viewmodels/fill_the_blanks_question_viewmodel.dart';
 import 'package:ez_english/features/models/base_question.dart';
+import 'package:ez_english/theme/palette.dart';
+import 'package:ez_english/theme/text_styles.dart';
+import 'package:ez_english/widgets/button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
-class FillTheBlanksForm extends StatelessWidget {
+class FillTheBlanksForm extends StatefulWidget {
   final String level;
   final String section;
   final String day;
@@ -17,23 +24,44 @@ class FillTheBlanksForm extends StatelessWidget {
     this.onSubmit,
   });
 
+  @override
+  State<FillTheBlanksForm> createState() => _FillTheBlanksFormState();
+}
+
+class _FillTheBlanksFormState extends State<FillTheBlanksForm> {
+  int englishBlankStart = 0;
+  int englishBlankEnd = 0;
+  int arabicBlankStart = 0;
+  int arabicBlankEnd = 0;
   final _formKey = GlobalKey<FormState>();
+
   final TextEditingController incompleteSentenceInEnglishController =
       TextEditingController();
+
   final TextEditingController incompleteSentenceInArabicController =
       TextEditingController();
-  final TextEditingController answerController = TextEditingController();
+
   final TextEditingController questionEnglishController =
       TextEditingController();
+
   final TextEditingController questionArabicController =
       TextEditingController();
-  void insertBlank(TextEditingController controller) {
-    final text = controller.text;
+
+  void insertBlank(TextEditingController controller,
+      {bool isEnglishField = true}) {
+    if (controller.selection ==
+        TextSelection(baseOffset: -1, extentOffset: -1)) {
+      return;
+    }
     final selection = controller.selection;
-    final newText = text.replaceRange(selection.start, selection.end, '_____');
-    controller.text = newText;
-    controller.selection =
-        TextSelection.fromPosition(TextPosition(offset: selection.start + 5));
+    if (isEnglishField) {
+      englishBlankStart = selection.start;
+      englishBlankEnd = selection.end;
+    } else {
+      arabicBlankStart = selection.start;
+      arabicBlankEnd = selection.end;
+    }
+    setState(() {});
   }
 
   @override
@@ -41,7 +69,7 @@ class FillTheBlanksForm extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (_) => FillTheBlanksViewModel(),
       child: Consumer<FillTheBlanksViewModel>(
-        builder: (context, viewModel, child) {
+        builder: (context, viewmodel, child) {
           return Form(
             key: _formKey,
             child: SingleChildScrollView(
@@ -50,15 +78,15 @@ class FillTheBlanksForm extends StatelessWidget {
                   TextFormField(
                     controller: incompleteSentenceInEnglishController,
                     decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Incomplete Sentence in English",
-                      hintText: "Enter the incomplete sentence in English",
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: () =>
-                            insertBlank(incompleteSentenceInEnglishController),
-                      ),
-                    ),
+                        border: const OutlineInputBorder(),
+                        labelText: "Full sentence (English)",
+                        hintText: "Ex: \"The boy kicks the ball.\"",
+                        suffixIcon: TextButton(
+                          onPressed: () {
+                            insertBlank(incompleteSentenceInEnglishController);
+                          },
+                          child: const Text("Insert blank"),
+                        )),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter the incomplete sentence in English';
@@ -66,34 +94,98 @@ class FillTheBlanksForm extends StatelessWidget {
                       return null;
                     },
                   ),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    controller: incompleteSentenceInArabicController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Incomplete Sentence in Arabic",
-                      hintText: "Enter the incomplete sentence in Arabic",
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: () =>
-                            insertBlank(incompleteSentenceInArabicController),
-                      ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    width: double.infinity,
+                    height: 70.h,
+                    child: Column(
+                      children: [
+                        Text.rich(
+                          style: TextStyles.bodyLarge,
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text: incompleteSentenceInEnglishController.text
+                                    .substring(0, englishBlankStart),
+                                style: const TextStyle(color: Colors.black),
+                              ),
+                              TextSpan(
+                                text: '_____',
+                                style:
+                                    const TextStyle(color: Palette.primaryText),
+                              ),
+                              TextSpan(
+                                text: incompleteSentenceInEnglishController.text
+                                    .substring(englishBlankEnd),
+                                style: const TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                            "Answer: ${incompleteSentenceInEnglishController.text.substring(englishBlankStart, englishBlankEnd)}",
+                            style: TextStyles.bodyLarge)
+                      ],
                     ),
                   ),
                   const SizedBox(height: 10),
                   TextFormField(
-                    controller: answerController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Answer",
-                      hintText: "Enter the answer",
+                    controller: incompleteSentenceInArabicController,
+                    decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: "Full sentence (Arabic)",
+                        hintText: "Enter the incomplete sentence in Arabic",
+                        suffixIcon: TextButton(
+                          onPressed: () {
+                            insertBlank(incompleteSentenceInArabicController,
+                                isEnglishField: false);
+                          },
+                          child: const Text("Insert blank"),
+                        )),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter the answer';
-                      }
-                      return null;
-                    },
+                    width: double.infinity,
+                    height: 70.h,
+                    child: Column(
+                      children: [
+                        Text.rich(
+                          style: TextStyles.bodyLarge,
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text: incompleteSentenceInArabicController.text
+                                    .substring(0, arabicBlankStart),
+                                style: const TextStyle(color: Colors.black),
+                              ),
+                              TextSpan(
+                                text: '_____',
+                                style:
+                                    const TextStyle(color: Palette.primaryText),
+                              ),
+                              TextSpan(
+                                text: incompleteSentenceInArabicController.text
+                                    .substring(arabicBlankEnd),
+                                style: const TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                            "Answer: ${incompleteSentenceInArabicController.text.substring(arabicBlankStart, arabicBlankEnd)}",
+                            style: TextStyles.bodyLarge)
+                      ],
+                    ),
                   ),
                   const SizedBox(height: 10),
                   TextFormField(
@@ -116,12 +208,17 @@ class FillTheBlanksForm extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 10),
-                  ElevatedButton(
+                  Button(
                     onPressed: () async {
                       if (_formKey.currentState!.validate()) {
-                        final question = await viewModel.submitForm(
-                          incompleteSentenceInEnglish:
-                              incompleteSentenceInEnglishController.text.trim(),
+                        final text =
+                            incompleteSentenceInEnglishController.text.trim();
+
+                        final newText = text.replaceRange(
+                            englishBlankStart, englishBlankEnd, '_____');
+                        viewmodel
+                            .submitForm(
+                          incompleteSentenceInEnglish: newText,
                           incompleteSentenceInArabic:
                               incompleteSentenceInArabicController.text
                                       .trim()
@@ -129,34 +226,56 @@ class FillTheBlanksForm extends StatelessWidget {
                                   ? null
                                   : incompleteSentenceInArabicController.text
                                       .trim(),
-                          answer: answerController.text.trim(),
+                          answer: incompleteSentenceInEnglishController.text
+                              .substring(englishBlankStart, englishBlankEnd)
+                              .trim(),
                           questionTextInEnglish:
                               questionEnglishController.text.trim(),
                           questionTextInArabic:
                               questionArabicController.text.trim().isEmpty
                                   ? null
                                   : questionArabicController.text.trim(),
-                        );
-                        if (question != null) {
-                          if (onSubmit != null) {
-                            onSubmit!(question);
-                          } else {
-                            await viewModel.uploadQuestion(
-                              level: level,
-                              section: section,
-                              day: day,
-                              question: question,
-                            );
-                          }
-                          print("Question added to Firebase");
-                        } else {
-                          print("Form validation failed.");
-                        }
+                        )
+                            .then((question) {
+                          showConfirmSubmitModalSheet(
+                              context: context,
+                              onSubmit: () async {
+                                await viewmodel.uploadQuestion(
+                                  level: widget.level,
+                                  section: widget.section,
+                                  day: widget.day,
+                                  question: question!,
+                                );
+                              },
+                              question: question);
+                        });
+                        // if (question != null) {
+                        // if (widget.onSubmit != null) {
+                        //   widget.onSubmit!(question);
+                        // }
+                        // else {
+                        //   showConfirmSubmitModalSheet(
+                        //       context: context,
+                        //       onSubmit: () async {
+                        //         await viewmodel.uploadQuestion(
+                        //           level: widget.level,
+                        //           section: widget.section,
+                        //           day: widget.day,
+                        //           question: question,
+                        //         );
+                        //       },
+                        //       question: question);
+                        // }
+                        //   print("Question added to Firebase");
+                        // }
+                        //  else {
+                        //   print("Form validation failed.");
+                        // }
                       } else {
                         print("Please fill all the required fields");
                       }
                     },
-                    child: const Text("Submit"),
+                    text: "Submit",
                   ),
                 ],
               ),
