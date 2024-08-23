837a596960db346cd3a31bab3282c88dd66ca513
Mohammed <bgd4500@gmail.com>
2024-08-23 16:11:58 +0300

    feature: add question assignment screen UI

--- lib/features/home/admin/question_assignment/question_assignment.dart
+++ lib/features/home/admin/question_assignment/question_assignment.dart
@@ -1,52 +1,177 @@
+import 'package:ez_english/core/constants.dart';
+import 'package:ez_english/features/home/admin/question_assignment/question_assignment_viewmodel.dart';
+import 'package:ez_english/features/home/content/data_entry_forms/dictation_question_form.dart';
 import 'package:ez_english/features/models/base_question.dart';
 import 'package:ez_english/theme/palette.dart';
+import 'package:ez_english/widgets/text_field.dart';
+import 'package:ez_english/widgets/vertical_list_item_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
-class QuestionAssignment extends StatelessWidget {
+class QuestionAssignment extends StatefulWidget {
   final String userId;
-  const QuestionAssignment({super.key, required this.userId});
+
+  QuestionAssignment({super.key, required this.userId});
+
+  @override
+  State<QuestionAssignment> createState() => _QuestionAssignmentState();
+}
+
+class _QuestionAssignmentState extends State<QuestionAssignment> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final viewmodel =
+          Provider.of<QuestionAssignmentViewmodel>(context, listen: false);
+      viewmodel.setValuesAndInit(
+        userId: widget.userId,
+      );
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            context.pop();
-          },
-          icon: const Icon(Icons.arrow_back),
-          color: Palette.primaryText,
+    return Consumer<QuestionAssignmentViewmodel>(
+      builder: (context, viewmodel, _) => Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              viewmodel.reset();
+              context.pop();
+            },
+            icon: const Icon(Icons.arrow_back),
+            color: Palette.primaryText,
+          ),
+          title: const Text(
+            'User Settings',
+            style: TextStyle(color: Palette.primaryText),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
         ),
-        title: const Text(
-          'User Settings',
-          style: TextStyle(color: Palette.primaryText),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+        body: Padding(
+          padding: EdgeInsets.all(Constants.padding12),
+          child: Column(
             children: [
-              DropdownButton<String>(items: const [
-                DropdownMenuItem(
-                  value: "mcq",
-                  child: Text("mcq"),
+              CustomTextField(
+                controller: _searchController,
+                hintText: "Search questions",
+                onChanged: (String query) {
+                  viewmodel.updateAndFilter(query: query);
+                },
+                textInputAction: TextInputAction.search,
+                trailingIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    viewmodel.updateAndFilter(query: _searchController.text);
+                  },
                 ),
-                DropdownMenuItem(
-                  value: "child",
-                  child: Text("child"),
-                )
-              ], hint: const Text("Question type"), onChanged: (value) {}),
-              DropdownButton<QuestionType>(
-                  items: const [],
-                  hint: const Text("Question section"),
-                  onChanged: (value) {})
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  // question type selector
+                  Expanded(
+                    child: DropdownButton<String>(
+                      value: viewmodel.selectedQuestionType,
+                      items: const [
+                        DropdownMenuItem(
+                          value: "mcq",
+                          child: Text("mcq"),
+                        ),
+                        DropdownMenuItem(
+                          value: "child",
+                          child: Text("child"),
+                        )
+                      ],
+                      hint: const Text("Question type"),
+                      onChanged: (value) {
+                        viewmodel.updateAndFilter(selectedQuestionType: value);
+                      },
+                    ),
+                  ),
+
+                  // question section selector
+                  Expanded(
+                    child: DropdownButton<String>(
+                      value: viewmodel.selectedSection,
+                      items: const [
+                        DropdownMenuItem(
+                          value: "reading",
+                          child: Text("reading"),
+                        ),
+                        DropdownMenuItem(
+                          value: "writing",
+                          child: Text("writing"),
+                        ),
+                        DropdownMenuItem(
+                          value: "listening",
+                          child: Text("listening"),
+                        ),
+                        DropdownMenuItem(
+                          value: "speaking",
+                          child: Text("speaking"),
+                        ),
+                      ],
+                      hint: const Text("Question section"),
+                      onChanged: (value) {
+                        viewmodel.updateAndFilter(selectedSection: value);
+                      },
+                    ),
+                  )
+                ],
+              ),
+              Expanded(
+                  child: viewmodel.isLoading
+                      ? const Center(child: CircularProgressIndicator())
+                      : viewmodel.questions.isNotEmpty
+                          ? ListView.builder(
+                              itemCount: viewmodel.filteredQuestions.length,
+                              itemBuilder: (context, index) {
+                                BaseQuestion question =
+                                    viewmodel.questions[index];
+                                bool isAssigned = viewmodel.assignedQuestions
+                                    .contains(question);
+                                return VerticalListItemCard(
+                                  action: isAssigned ? Icons.delete : Icons.add,
+                                  onIconPressed: () {
+                                    isAssigned
+                                        ? viewmodel.removeQuestion(question)
+                                        : viewmodel.assignQuestion(question);
+                                  },
+                                  onTap: () {
+                                    showPreviewModalSheet(
+                                        title: "Question Preview",
+                                        context: context,
+                                        question: viewmodel.questions[index],
+                                        onSubmit: null,
+                                        showSubmitButton: false);
+                                  },
+                                  showDeleteIcon: false,
+                                  mainText:
+                                      "${index + 1}. ${viewmodel.questions[index].questionTextInEnglish ?? "No question text"}",
+                                  info: Text(viewmodel
+                                          .questions[index].titleInEnglish ??
+                                      ""),
+                                  subText: viewmodel
+                                      .questions[index].questionType
+                                      .toShortString(),
+                                );
+                              },
+                            )
+                          : const Text(
+                              "No questions found for the selected day."))
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
