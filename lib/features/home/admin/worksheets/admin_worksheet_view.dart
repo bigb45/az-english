dc0852363aea879c7286f24211b876fd4f553f9a
Mohammed <bgd4500@gmail.com>
2024-09-07 10:34:31 +0300

    feature: add modal bottom sheet to view original worksheet

--- lib/features/home/admin/worksheets/admin_worksheet_view.dart
+++ lib/features/home/admin/worksheets/admin_worksheet_view.dart
@@ -3,11 +3,12 @@ import 'package:ez_english/features/home/admin/worksheets/worksheets_viewmodel.d
 import 'package:ez_english/theme/palette.dart';
 import 'package:ez_english/theme/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 class AdminWorksheetView extends StatelessWidget {
   final String worksheetId;
-  AdminWorksheetView({super.key, required this.worksheetId});
+  const AdminWorksheetView({super.key, required this.worksheetId});
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +19,60 @@ class AdminWorksheetView extends StatelessWidget {
       return Scaffold(
         appBar: AppBar(
           iconTheme: const IconThemeData(color: Palette.primaryText),
-          title: const Text(
-            'Administrator',
-            style: TextStyle(color: Palette.primaryText),
+          title: Text(
+            viewmodel.worksheets[worksheetIndex].title ?? "Worksheet view",
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(color: Palette.primaryText),
           ),
+          actions: [
+            GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                    context: context,
+                    enableDrag: true,
+                    builder: (context) {
+                      return Center(
+                        child: InteractiveViewer(
+                          child: CachedNetworkImage(
+                            progressIndicatorBuilder: (context, url, progress) {
+                              return Center(
+                                child: CircularProgressIndicator(
+                                  value: progress.progress,
+                                ),
+                              );
+                            },
+                            imageUrl:
+                                viewmodel.worksheets[worksheetIndex].imageUrl ??
+                                    "",
+                          ),
+                        ),
+                      );
+                    });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Palette.secondary,
+                  border: Border.all(color: Palette.secondaryStroke, width: 2),
+                  borderRadius: BorderRadius.circular(16.r),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Palette.secondaryStroke,
+                      offset: Offset(0, 3),
+                      blurRadius: 0,
+                    ),
+                  ],
+                ),
+                width: 70.w,
+                height: 70.h,
+                child: const Center(
+                  child: Icon(
+                    Icons.task,
+                    color: Palette.secondaryText,
+                  ),
+                ),
+              ),
+            ),
+          ],
           centerTitle: true,
           backgroundColor: Colors.transparent,
         ),
