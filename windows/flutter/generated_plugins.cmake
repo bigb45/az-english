93999c6a41f9c92f4677d6e6c5820886267c146a
ribhy-bishtawi <ribhy2001@gmail.com>
2024-10-04 12:16:58 +0300

    fix: resolve Flutter errors after Xcode update

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -9,6 +9,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
   firebase_storage
+  flutter_inappwebview_windows
   flutter_tts
   permission_handler_windows
   url_launcher_windows
