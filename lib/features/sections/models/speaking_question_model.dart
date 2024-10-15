17c8467234903d15460a09362a10ade99861ca24
ribhy-bishtawi <ribhy2001@gmail.com>
2024-10-15 23:11:53 +0300

    feat: Initialize speaking question class with essential methods and variables

--- lib/features/sections/models/speaking_question_model.dart
+++ lib/features/sections/models/speaking_question_model.dart
@@ -1,20 +1,60 @@
 import 'package:ez_english/features/models/base_question.dart';
 import 'package:ez_english/features/sections/models/string_answer.dart';
+import 'package:flutter/foundation.dart';
 
 class SpeakingQuestionModel extends BaseQuestion<String> {
   String question;
-  String? audioUrl;
-  final String correctAnswer;
-  SpeakingQuestionModel(
-      {required this.question, required this.correctAnswer, this.audioUrl})
-      : super(
-            titleInEnglish: null,
-            questionTextInEnglish: question,
-            questionTextInArabic: question,
-            answer: StringAnswer(answer: correctAnswer),
-            imageUrl: '',
-            voiceUrl: audioUrl ?? "",
-            questionType: QuestionType.speaking);
+  SpeakingQuestionModel({
+    required this.question,
+    super.titleInEnglish,
+    super.questionTextInEnglish,
+    super.questionTextInArabic,
+    super.questionType = QuestionType.speaking,
+    super.imageUrl,
+    super.voiceUrl,
+    super.sectionName,
+  });
+
+  @override
+  SpeakingQuestionModel copy() {
+    return SpeakingQuestionModel(
+      question: question,
+      questionTextInArabic: questionTextInArabic,
+      questionTextInEnglish: questionTextInEnglish,
+      imageUrl: imageUrl,
+      voiceUrl: voiceUrl,
+      questionType: questionType,
+      titleInEnglish: titleInEnglish,
+      sectionName: sectionName,
+    );
+  }
+
+  factory SpeakingQuestionModel.fromMap(Map<String, dynamic> map) {
+    return SpeakingQuestionModel(
+      question: map['question'],
+      questionTextInEnglish: map['questionTextInEnglish'],
+      questionTextInArabic: map['questionTextInArabic'],
+      voiceUrl: map['voiceUrl'],
+      imageUrl: map['imageUrl'],
+      titleInEnglish: map["titleInEnglish"],
+      sectionName: SectionNameExtension.fromString(map['sectionName']),
+    );
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> baseMap = super.toMap();
+    return {
+      ...baseMap,
+      'question': question,
+    };
+  }
+
+  @override
+  bool evaluateAnswer() {
+    return true;
+  }
+
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
@@ -22,51 +62,15 @@ class SpeakingQuestionModel extends BaseQuestion<String> {
 
     return other.runtimeType == runtimeType &&
         other.question == question &&
-        other.audioUrl == audioUrl &&
-        other.correctAnswer == correctAnswer &&
-        other.questionTextInEnglish ==
-            questionTextInEnglish && // From BaseQuestion
-        other.questionTextInArabic ==
-            questionTextInArabic && // From BaseQuestion
-        other.imageUrl == imageUrl && // From BaseQuestion
-        other.voiceUrl == voiceUrl && // From BaseQuestion
-        other.titleInEnglish == titleInEnglish && // From BaseQuestion
-        other.questionType == questionType; // From BaseQuestion
+        other.questionTextInEnglish == questionTextInEnglish &&
+        other.questionTextInArabic == questionTextInArabic &&
+        other.imageUrl == imageUrl &&
+        other.voiceUrl == voiceUrl &&
+        other.titleInEnglish == titleInEnglish;
   }
 
   @override
-  int get hashCode => Object.hash(
-        runtimeType,
-        question,
-        audioUrl,
-        correctAnswer,
-        questionTextInEnglish, // From BaseQuestion
-        questionTextInArabic, // From BaseQuestion
-        imageUrl, // From BaseQuestion
-        voiceUrl, // From BaseQuestion
-        titleInEnglish, // From BaseQuestion
-        questionType, // From BaseQuestion
+  int get hashCode => Object.hash(runtimeType, question,
+      super.hashCode // Including all properties from the BaseQuestion
       );
-
-  @override
-  BaseQuestion<String> copy() {
-    // TODO: implement copy
-    throw UnimplementedError();
-  }
-
-// TODO: make sure this is correct
-  // factory SpeakingQuestionModel.fromJson(Map<String, dynamic> json) {
-  //   return SpeakingQuestionModel(
-  //       question: json['question'],
-  //       correctAnswer: json['answer'],
-  //       audioUrl: json['audioUrl']);
-  // }
-
-  // Map<String, dynamic> toJson() {
-  //   return {
-  //     'question': question,
-  //     'answer': correctAnswer,
-  //     'audioUrl': audioUrl
-  //   };
-  // }
 }
